bee22dd94ec6640caf0de79996632e79546e3204
alexey <alexey@mainpc.the-island.ru>
2012-01-11 23:17:33 +0800

    Еще более случайные цвета! Яркость

--- lib/color.py
+++ lib/color.py
@@ -69,12 +69,13 @@ class RandomColorGenerator(object):
         self.colorMap = {}
         keys = self.colors.keys()
         random.shuffle(keys)
+        start = random.uniform(-.5/colorsCount, .5 / colorsCount)
         for colorIndex, key in enumerate(keys):
             color = hsv2hex(
-                float(colorIndex)/colorsCount
-                    + random.uniform(-.25/colorsCount, .25/colorsCount),
-                random.uniform(.3,.6),
-                random.uniform(.6,.9)
+                (1. + start + float(colorIndex)/colorsCount
+                    + random.uniform(-.25/colorsCount, .25/colorsCount)) % 1.0,
+                random.uniform(.6,.8),
+                random.uniform(.75,.9)
             )
 
             self.colorMap[key] = color
